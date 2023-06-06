dc8760007453576acf6014f0197b21587a20eead
Rojhat Algunerhan <roal005@student.kristiania.no>
2023-06-06 12:12:15 +0200

    Creating User dummy model and Comment model

---
+++ src/main/java/Comment.java
@@ -0,0 +1,13 @@
+public class Comment {
+    private int id;
+    private String text;
+    private User user;
+
+    public Comment(int id, String text, User user) {
+        this.id = id;
+        this.text = text;
+        this.user = user;
+    }
+
+    // Getters and setters
+}
