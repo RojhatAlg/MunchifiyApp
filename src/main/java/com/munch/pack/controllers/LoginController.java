93a25a51a44eacdfbbc145741105fe934434a8b4
Mariusflores <mada060@student.kristiania.no>
2023-06-13 13:04:52 +0200

    updated logincontroller

--- src/main/java/com/munch/pack/controllers/LoginController.java
+++ src/main/java/com/munch/pack/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.munch.pack.controllers;
 
 import com.munch.pack.dao.LoginDao;
+import com.munch.pack.dao.UserDao;
 import com.munch.pack.entities.Login;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,19 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/login")
 public class LoginController {
     private LoginDao loginDao;
+    private UserDao userdao;
 
     public LoginController(){
         loginDao = new LoginDao();
+
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping
-    public ResponseEntity<String> authenticateUser(@RequestBody Login login){
+    public ResponseEntity<Long> authenticateUser(@RequestBody Login login){
+
 
         if (loginDao.authenticate(login)){
-            return new ResponseEntity<>("User signed in successfully", HttpStatus.OK);
+            long id = login.getId();
+            return ResponseEntity.ok(id);
         }else{
-            return new ResponseEntity<>("Failed to login", HttpStatus.UNAUTHORIZED);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body((long)-1);
         }
     }
 
